a5951cd2e019b33ecf2c755c70671373675ed4d5
Carlos  S <carlos.schipper@gmail.com>
2023-06-24 01:47:06 +0200

    added foldersize + code improvements

--- findfolder_improved.ps1
+++ findfolder_improved.ps1
@@ -1,4 +1,12 @@
 $findFolder = "node_modules";
-$inPath = "C:\"; 
+$inPath = "P:\dev\";
 $depth = 6;
-Get-ChildItem -Path $inPath -Directory -Depth $depth | Where-Object { $_.Name -eq $findFolder } | ForEach-Object { Write-Host $_.FullName }
+$total = 0;
+$folderPaths = (Get-ChildItem -Path $inPath -Directory -Filter $findFolder -Depth $depth -Force -ErrorAction SilentlyContinue).FullName;
+foreach ($folderPath in $folderPaths)
+{ 
+    $total += (Get-ChildItem -Path $folderPath -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum).Sum;
+    $folderSizeFormatted = "{0:N2}" -f ((Get-ChildItem -Path $folderPath -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum).Sum / 1MB); 
+    "Size: $folderSizeFormatted MB - $folderPath" 
+};
+"totalSize: {0:N2} MB" -f ($total / 1MB)
